c48356f508d00b5c6e2e02eb1002c2051442f039
xrr2016 <xiaoranran1993@163.com>
2017-11-12 22:57:13 +0800

    :zap: style

---
+++ src/hinter.js
@@ -0,0 +1,20 @@
+const hinter = {
+  log () {
+    console.log('hinter log')
+  },
+  info () {
+    console.info('hinter info')
+  },
+  warn () {
+    console.warn('hinter warn')    
+  },
+  error () {
+    console.error('hinter error')    
+  },
+  success () {
+    console.log('hinter success')    
+  },
+  clear () {
+    console.log('hinter clear')    
+  }
+}
