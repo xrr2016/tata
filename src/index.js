2dbe53ce4e71464a027ef391bafa26464839cb6b
xrr2016 <xiaoranran1993@163.com>
2017-11-24 00:21:09 +0800

    :sparkles:  improve

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import './index.css'
-// import tata from '../dist/tata'
+import tata from '../dist/tata'
 
-// tata.success()
+const footer = document.querySelector('footer')
+
+footer.addEventListener('click', (event) => {
+  const target = event.target
+  const type = target.dataset.type
+
+  if (!type) return
+
+  tata[type]()
+})
--- src/tata.js
+++ src/tata.js
@@ -7,16 +7,51 @@ document.head.appendChild(iconLink)
 const defaultOpts = {
   type: 'log',
   position: 'top-right',
-  duration: 3000,
+  duration: 5000,
   progress: true,
   progressWidth: 100,
   holding: false,
   icon: true,
   closeBtn: true,
-  show: 'fadeIn', // slideIn
-  hide: 'fadeOut', // slideOut
+  show: 'fadeIn',
+  hide: 'fadeOut',
   onClick: null,
   onClose: null
+}
+
+const tatas = []
+const tata = {
+  text(text = '你好, 今天是' + new Date().toLocaleString(), opts = {}) {
+    const _opts = Object.assign({}, defaultOpts, opts, { type: 'text' })
+    const title = ''
+    render(title, text, _opts)
+  },
+  log(title = '你好', text = '今天是' + new Date().toLocaleString(), opts = {}) {
+    const _opts = Object.assign({}, defaultOpts, opts, { type: 'log' })
+    render(title, text, _opts)
+  },
+  info(title = '你好', text = '今天是' + new Date().toLocaleString(), opts = {}) {
+    const _opts = Object.assign({}, defaultOpts, opts, { type: 'info' })
+    render(title, text, _opts)
+  },
+  warn(title = '你好', text = '今天是' + new Date().toLocaleString(), opts = {}) {
+    const _opts = Object.assign({}, defaultOpts, opts, { type: 'warn' })
+    render(title, text, _opts)
+  },
+  error(title = '你好', text = '今天是' + new Date().toLocaleString(), opts = {}) {
+    const _opts = Object.assign({}, defaultOpts, opts, { type: 'error' })
+    render(title, text, _opts)
+  },
+  success(title = '你好', text = '今天是' + new Date().toLocaleString(), opts = {}) {
+    const _opts = Object.assign({}, defaultOpts, opts, { type: 'success' })
+    render(title, text, _opts)
+  },
+  clear() {
+    tatas.forEach(tata => {
+      document.getElementById(tata.id).style.display = 'none'
+    })
+    tatas.length = 0
+  }
 }
 
 function type2Icon(type) {
@@ -38,52 +73,59 @@ function randomId() {
   return `tata-${Date.now()}`
 }
 
-const tata = {
-  tatas: [],
-  text(text = '你好, 今天是' + new Date().toLocaleString(), opts = {}) {
-    const _opts = Object.assign(defaultOpts, opts, {
-      type: 'text'
-    })
-    tata._render(title, text, _opts)
-  },
-  log(title = '你好', text = '今天是' + new Date().toLocaleString(), opts = {}) {
-    const _opts = Object.assign(defaultOpts, opts, {
-      type: 'log'
-    })
-    tata._render(title, text, _opts)
-  },
-  info(title = '你好', text = '今天是' + new Date().toLocaleString(), opts = {}) {
-    const _opts = Object.assign(defaultOpts, opts, {
-      type: 'info'
-    })
-    tata._render(title, text, _opts)
-  },
-  warn(title = '你好', text = '今天是' + new Date().toLocaleString(), opts = {}) {
-    const _opts = Object.assign(defaultOpts, opts, {
-      type: 'warn'
-    })
-    tata._render(title, text, _opts)
-  },
-  error(title = '你好', text = '今天是' + new Date().toLocaleString(), opts = {}) {
-    const _opts = Object.assign(defaultOpts, opts, {
-      type: 'error'
-    })
-    tata._render(title, text, _opts)
-  },
-  success(title = '你好', text = '今天是' + new Date().toLocaleString(), opts = {}) {
-    const _opts = Object.assign(defaultOpts, opts, {
-      type: 'success'
-    })
-    tata._render(title, text, _opts)
-  },
-  _close(event) {
-    const target = event.target
-    if (!target.classList.contains('tata-close')) return
-    const id = target.parentNode.getAttribute('id')
-    const tata = tata.tatas.find(tata => tata.id === id)
-    const idx = tata.tatas.findIndex(tata => id === tata.id)
-    const hideClass = tata.opts.hide === 'fadeOut' ? 'fade-out' : 'slide-out'
-    const element = document.getElementById(id)
+function moveDown() {
+  const len = tatas.length - 1
+  const element = document.getElementById(this.id)
+  const eleHeight = element.getBoundingClientRect().height
+  document.getElementById(this.id).style.top = `${len * eleHeight + 24}px`
+}
+
+function click(event) {
+  const target = event.target
+  if (target.classList.contains('tata-close')) return
+  this.opts.onClick.call(this)
+}
+
+function closeTaTa(event) {
+  const target = event.target
+  if (!target.classList.contains('tata-close')) return
+  const id = target.parentNode.getAttribute('id')
+  const ta = tatas.find(ta => ta.id === id)
+  const element = document.getElementById(id)
+
+  const timeout = setTimeout(() => {
+    if (typeof element.remove === 'function') {
+      element.remove()
+    } else {
+      document.body.removeChild(element)
+    }
+    clearTimeout(timeout)
+  }, 800)
+  
+  element.classList.add('fade-out')
+
+  !!ta.opts.onClose &&
+    typeof ta.opts.onClose === 'function' &&
+    ta.opts.onClose.call(ta)
+}
+
+document.addEventListener('click', closeTaTa, false)
+
+function reduceProgress() {
+  const id = arguments[0]
+  const ta = tatas.find(ta => ta.id === id)
+  const element = document.getElementById(id)
+  const progress = element.querySelector('.tata-progress')
+
+  ta.opts.duration -= ta.opts.duration / 100
+  ta.opts.progressWidth -= 0.6
+  progress.style.width = ta.opts.progressWidth + '%'
+  const reduce = requestAnimationFrame(reduceProgress.bind(ta, ta.id))
+
+  if (ta.opts.progressWidth <= 0) {
+    const idx = tatas.findIndex(tata => tata === ta)
+    tatas.splice(idx, 1)
+
     const timeout = setTimeout(() => {
       if (typeof element.remove === 'function') {
         element.remove()
@@ -92,102 +134,64 @@ const tata = {
       }
       clearTimeout(timeout)
     }, 800)
-    element.classList.add(hideClass)
-    tata.tatas.splice(idx, 1)
-    !!tata.opts.onClose &&
-      typeof tata.opts.onClose === 'function' &&
-      tata.opts.onClose.call(tata)
-  },
-  _click(event) {
-    const target = event.target
-    if (target.classList.contains('tata-close')) return
-    this.opts.onClick.call(this)
-  },
-  clear() {
-    tata.tatas.forEach(tata => {
-      document.getElementById(tata.id).style.display = 'none'
-    })
-    tata.tatas.length = 0
-  },
-  _render(title, text, opts) {
-    const icon = type2Icon(opts.type)
-    const id = randomId()
-    const times = opts.duration / (1000 / 60)
-    const ms = opts.duration / times
-    Object.assign(opts, { reduceTimes: times, perReduceMs: ms })
-    const ta = { title, text, opts, id }
-    tata.tatas.push(ta)
-    const idx = tata.tatas.findIndex(tata => tata.id === id)
-    const prevtata = tata.tatas[idx - 1]
-    const show = opts.show === 'fadeIn' ? 'fade-in' : 'slide-in'
-    const template = `
-      <div class="tata ${opts.position} ${opts.type} ${show}" id=${id}>
-        <i class="tata-icon material-icons">${icon}</i>
-        <div class="tata-body">
-          <h4 class="tata-title">${title}</h4>
-          <p class="tata-text">${text}</p>
-        </div>
-        ${opts.closeBtn
-          ? '<button class="tata-close material-icons">clear</button>'
-          : ''}
-        ${!opts.holding && opts.progress
-          ? '<div class="tata-progress"></div>'
-          : ''}
-      </div>
-    `
-    document.body.insertAdjacentHTML('beforeend', template)
-    if (prevtata && prevtata.opts.position === ta.opts.position && document.getElementById(prevtata.id)) {
-      tata._moveDown.call(prevtata)
-    }
-    !!opts.onClick &&
-      typeof opts.onClick === 'function' &&
-      document
-        .getElementById(id)
-        .addEventListener('click', tata._click.bind(tata), {
-          capture: true,
-          once: true
-        })
-    !opts.holding &&
-      opts.progress &&
-      requestAnimationFrame(tata._reduceProgress.bind(ta, ta.id))
-    return false
-  },
-  _moveDown() {
-    const element = document.getElementById(this.id)
-    const len = tata.tatas.length - 1
-    const eleHeight = element.getBoundingClientRect().height
-    document.getElementById(this.id).style.top = `${len * eleHeight + 24}px`
-  },
-  _reduceProgress() {
-    const ta = tata.tatas.find(tata => tata.id === arguments[0])
-    const id = ta.id
-    const progress = document.getElementById(id).querySelector('.tata-progress')
-    ta.opts.progressWidth -= 100 / ta.opts.reduceTimes
-    ta.opts.duration -= ta.opts.perReduceMs
-    progress.style.width = ta.opts.progressWidth + '%'
-    const cut = requestAnimationFrame(tata._reduceProgress.bind(tata, id))
-    if (ta.opts.progressWidth <= 0) {
-      const idx = tata.tatas.findIndex(tata => tata === tata)
-      const element = document.getElementById(id)
-      tata.tatas.splice(idx, 1)
-      const timeout = setTimeout(() => {
-        if (typeof element.remove === 'function') {
-          element.remove()
-        } else {
-          document.body.removeChild(element)
-        }
-        clearTimeout(timeout)
-      }, 800)
-      !!ta.opts.onClose &&
-        typeof ta.opts.onClose === 'function' &&
-        ta.opts.onClose.call(tata)
-      cancelAnimationFrame(cut)
-    }
+
+    !!ta.opts.onClose &&
+      typeof ta.opts.onClose === 'function' &&
+      ta.opts.onClose.call(ta)
+
+      cancelAnimationFrame(reduce)
+      console.log(performance.now())
   }
 }
 
-if (tata.tatas.length) {
-  document.addEventListener('click', tata._close)
+function render(title, text, opts) {
+  const icon = type2Icon(opts.type)
+  const id = randomId()
+  const ta = { title, text, opts, id }
+  console.dir(ta)
+  const idx = tatas.findIndex(tata => tata.id === id)
+  const prevtata = idx === 0 ? null : tatas[idx - 1]
+  const show = opts.show === 'fadeIn' ? 'fade-in' : 'slide-in'
+
+  tatas.push(ta)
+
+  const template = `
+    <div class="tata ${opts.position} ${opts.type} ${show}" id=${id}>
+      <i class="tata-icon material-icons">${icon}</i>
+      <div class="tata-body">
+        <h4 class="tata-title">${title}</h4>
+        <p class="tata-text">${text}</p>
+      </div>
+      ${opts.closeBtn
+        ? '<button class="tata-close material-icons">clear</button>'
+        : ''}
+      ${!opts.holding && opts.progress
+        ? '<div class="tata-progress"></div>'
+        : ''}
+    </div>
+  `
+
+  document.body.insertAdjacentHTML('beforeend', template)
+
+  if (
+    prevtata &&
+    prevtata.opts.position === ta.opts.position &&
+    document.getElementById(prevtata.id)
+  ) {
+    moveDown.call(prevtata)
+  }
+
+  !!opts.onClick &&
+    typeof opts.onClick === 'function' &&
+    document.getElementById(id).addEventListener('click', click.bind(ta), {
+      capture: true,
+      once: true
+    })
+
+  console.log(performance.now())
+  !opts.holding &&
+    opts.progress &&
+    requestAnimationFrame(reduceProgress.bind(ta, ta.id))
 }
 
 module.exports = tata
